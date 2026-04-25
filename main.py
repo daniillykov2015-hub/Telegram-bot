165b8855d5e3c742df4542e3828284cbcea127d4
daniillykov2015-hub <daniil.lykov.2015@mail.ru>
2026-04-25 23:44:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -374,7 +374,8 @@ async def start(message: Message):
 @router.callback_query(F.data == "back")
 async def back(call: CallbackQuery):
     await call.message.edit_text(MAIN_TEXT, reply_markup=main_menu_kb())
-# --- PLATEGA ---@router.callback_query(F.data.startswith("card_confirm:"))
+# --- PLATEGA ---@
+router.callback_query(F.data.startswith("card_confirm:"))
 async def card_confirm(call: CallbackQuery):
     plan_id = call.data.split(":")[1]
     plan = PLANS.get(plan_id)
